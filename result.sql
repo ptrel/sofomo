dc11635122ccdb28914b66c7f95db4bb9ba58c6c
ptrel <ptrela@dyvenia.com>
2023-11-21 11:14:46 +0100

    handling null values

--- result.sql
+++ result.sql
@@ -40,14 +40,21 @@ table_union AS
 		0 AS measure_1,
 		measure_2
 	FROM mapped_table_b
+), 
+
+aggregation AS (
+	SELECT 	
+		dimension_1,
+		dimension_2,
+		SUM(measure_1) AS measure_1,
+		SUM(measure_2) AS measure_2
+	FROM table_union
+	GROUP BY 1,2 
 )
 
-SELECT 	
-	dimension_1,
-	dimension_2,
-	SUM(measure_1) AS measure_1,
-	SUM(measure_2) AS measure_2
-FROM table_union
-GROUP BY 1,2 
-
-
+SELECT 
+	COALESCE(dimension_1, 'Unknown') AS dimension_1,
+	COALESCE(dimension_2, 'Unknown') AS dimension_1,
+	COALESCE(measure_1, 0) AS measure_1,
+	COALESCE(measure_2, 0) AS measure_2
+FROM aggregation
